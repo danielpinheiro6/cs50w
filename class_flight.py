5e700d57b6677f31616f6114d46a352f51c83de6
Daniel <danielpinheiro6@hotmail.com>
2023-06-22 17:05:33 -0300

    add Python Class

---
+++ class_flight.py
@@ -0,0 +1,10 @@
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+        
+    def add_passenger(self, n):
+        
+        
+
+flight = Flight(3)
---
+++ name.py
@@ -0,0 +1,16 @@
+# name = input('Name: ')
+# print(name)
+
+n = int(input('Digite um número: '))
+
+if n > 0:
+    print(f'{n} é positivo.')
+
+elif n < 0:
+    print(f'{n} é negativo.')
+
+else:
+    print('n é zero')
+
+    
+
